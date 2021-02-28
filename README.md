6b8c439b3a7f647b8b125db221d9392eedee00c0
zMendes <leonardom1808@gmail.com>
2021-02-28 13:07:07 -0300

    Calculadora done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # compiler
-Compilador desenvolvido para a matéria Lógica Computacional 
+Compilador desenvolvido para a matéria Lógica Computacional Entrega 1
+
+## Rodando
+Para testar a calculadora:  
+Exemplo 1:  
+```$ python3 main.py "2+ 5 -3"```  
+Exemplo 2:  
+```$ python3 main.py "12-32+100"```  
+Exemplo 3  
+```$ python3 main.py "58   + 10  - 2"```
---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+
+command = " ".join(sys.argv[1:])#.replace(" ", "")
+
+operator = False
+space = False
+
+result = 0
+if command[0].isnumeric == False or command[-1].isnumeric() == False:
+    if (command[0] != " " or command[-1] != " "):
+        raise KeyError
+
+number = "" # command[-1]
+start = 0
+for i in range(len(command)-1,-1,-1):
+
+    if i != 0 and i!= len(command)-1:    
+        if command[i] == " " and command[i-1].isnumeric() and command[i+1].isnumeric():
+            print(i)
+            raise KeyError
+    if command[i].isnumeric():
+        number+= command[i]
+    if command[i] == "+":
+        result += int(number[::-1])
+        number = ""
+        start = i
+    
+    if command[i] == "-":
+        result -= int(number[::-1])
+        number = ""
+        start = i
+
+result+= int(number[::-1])
+    
+print(result)

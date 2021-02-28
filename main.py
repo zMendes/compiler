faab1e886ce794bee4f37be4e4c2353474b7fcec
zMendes <leonardom1808@gmail.com>
2021-02-28 13:09:22 -0300

    Done

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ for i in range(len(command)-1,-1,-1):
 
     if i != 0 and i!= len(command)-1:    
         if command[i] == " " and command[i-1].isnumeric() and command[i+1].isnumeric():
-            print(i)
             raise KeyError
     if command[i].isnumeric():
         number+= command[i]
